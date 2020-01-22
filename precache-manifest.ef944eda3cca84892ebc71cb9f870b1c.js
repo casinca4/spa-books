5835de52b3998063f71b7fb89eba86660964236f
casinca4 <s.antonelli@web.de>
2020-01-22 17:17:57 +0100

    Updates

---
+++ precache-manifest.ef944eda3cca84892ebc71cb9f870b1c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "761a12a8f1223e146b62502f926b2d80",
+    "url": "/spa-books/index.html"
+  },
+  {
+    "revision": "2f318975a1920b0ddfc4",
+    "url": "/spa-books/static/css/main.5146a36f.chunk.css"
+  },
+  {
+    "revision": "abb58b24799cb559de42",
+    "url": "/spa-books/static/js/2.bb22ead3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/spa-books/static/js/2.bb22ead3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2f318975a1920b0ddfc4",
+    "url": "/spa-books/static/js/main.d9674254.chunk.js"
+  },
+  {
+    "revision": "b904f0c62fff286fe817",
+    "url": "/spa-books/static/js/runtime-main.027ebe28.js"
+  }
+]);
